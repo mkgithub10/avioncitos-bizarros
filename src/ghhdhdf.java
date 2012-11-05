0d01d0bfa8a5f93ad8340163b289e0a00c513963
turco807@gmail.com <turco807@gmail.com@653cbc3e-606a-c710-d45b-03dc09a298a7>
2012-11-05 05:16:34 +0000

    estoy haciendo huevadas para ver si entiendo esto

---
+++ src/ghhdhdf.java
@@ -0,0 +1,21 @@
+
+
+import java.awt.List;
+import java.util.ArrayList;
+public class ghhdhdf {
+
+	
+	
+	public static void main(String[] args) 
+	{ 
+		System.out.println("hola mundo");
+		List pepe =new List();
+	
+		pepe.add("papa");
+		pepe.add("papein");
+		System.out.println(pepe.getItem(1));	
+		System.out.println(pepe.getItem(0));
+	}
+	
+
+}
