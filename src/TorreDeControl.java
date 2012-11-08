853850055e85e68fdf745737960aa0e28d887c54
bdmau91@gmail.com <bdmau91@gmail.com@653cbc3e-606a-c710-d45b-03dc09a298a7>
2012-11-08 03:15:11 +0000

    Agregué TorreDeControl con dos métodos.

--- src/TorreDeControl.java
+++ src/TorreDeControl.java
@@ -1,4 +1,16 @@
 
 public class TorreDeControl {
 
+	private Aeronave avion;
+	
+	public TorreDeControl(Aeronave avion){
+		this.avion = avion;
+	}
+
+	public Posicion posicionAvion(){
+		return this.avion.devolverPosicion();
+			
+	}
+	
+	
 }
