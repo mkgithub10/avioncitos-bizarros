6e67aadee4e67a02e5b97c11fcf6292976770d1e
Kimochi <Kimochi@653cbc3e-606a-c710-d45b-03dc09a298a7>
2012-11-14 20:07:46 +0000

    ControlCambiado

--- src/ControlTerrestre.java
+++ src/ControlTerrestre.java
@@ -6,16 +6,29 @@ public class ControlTerrestre
 {
 
 	private ArrayList<TorreDeControl> listaTorres = new ArrayList<TorreDeControl>();
+	private Radar radar = new Radar();
 	
-	public void agregarPista(Pista pista)
+	private void setearTorreA(Pista pista)
 	{
 		TorreDeControl torre = new TorreDeControl(pista);
 		listaTorres.add(torre);
 	}
 	
+	private void adjuntarPistasNuevas(ArrayList<Pista> listaPista)
+	{
+		if (listaPista.size() != listaTorres.size())
+		{
+			ListIterator<Pista> iter = listaPista.listIterator();
+			while (iter.hasNext())
+			{
+				this.setearTorreA(iter.next());
+			}
+		} 
+	}
 	
 	public void actualizar()
 	{
+		this.adjuntarPistasNuevas(radar.obtenerPistas());
 		ListIterator<TorreDeControl> iterador = listaTorres.listIterator();
 		while (iterador.hasNext())
 		{
