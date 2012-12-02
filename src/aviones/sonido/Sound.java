4046cd8f7880053bdcd31db8ae254695cef61e3f
Kimochi <Kimochi@653cbc3e-606a-c710-d45b-03dc09a298a7>
2012-12-02 19:08:50 +0000

    Sonido

--- src/aviones/sonido/Sound.java
+++ src/aviones/sonido/Sound.java
@@ -1,30 +1,26 @@
 package aviones.sonido;
 
 import java.io.FileInputStream;
-	 
 import javazoom.jl.player.advanced.AdvancedPlayer;
 	 
-	public class Sound  {
-	 
+public class Sound  
+{
+	public Sound()
+	{    }
 	     
-	    public Sound()
-	    {
-         
+	
+	public static void playMusic()
+	{    
+		try 
+		{
+			AdvancedPlayer p = new AdvancedPlayer(new
+			FileInputStream("/sonidos/prueba.mp3"));
+			p.play();
 	    }
-	     
-	    public static void playMusic()
-	    {
-	         
-	            try {
-	                AdvancedPlayer p = new AdvancedPlayer(new
-	                        FileInputStream("C:/prueba.mp3"));
-	                p.play();
-	            } catch (Exception e) {
-	                // TODO Auto-generated catch block
-	                System.out.println("Cannot find the file specified.");
-	                 
-	            }
-	         
+		catch (Exception e)
+		{
+			System.out.println("Cannot find the file specified.");
 	    }
-	 
+	         
 	}
+}
